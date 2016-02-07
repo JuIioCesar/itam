b9e25a7a4d06eaf06682e756decd80ad9b0582d5
liliana <liliana.millan@gmail.com>
2016-02-06 23:38:22 -0600

    identify the correct hierarchy to suggest

--- tesis/suggestedTags.R
+++ tesis/suggestedTags.R
@@ -7,6 +7,8 @@ suppressPackageStartupMessages(library(stringr))
 load("../tags.df.matrix.RData")
 source("../auxiliary_functions.R")
 
+
+##
 getTags <- function(content) {
   content.vector.source <- VectorSource(content)
   corpus.content <- VCorpus(content.vector.source, 
@@ -50,11 +52,44 @@ getTags <- function(content) {
   ranking$original <- sapply(ranking$tag, function(x)
     tags.df$tag[grep(paste0("^",x,"$"), tags.df$clean.unique)[1]])
   
+  
   suggested.tags <- data.frame(suggested.tag=ranking$original,
                                bm25=ranking$bm25)
   
   suggested.tags
   
+  
+  
 }
 
 
+#once we have the suggested tags we refine the hierarchy to suggest
+#if 60% of the terms in the hierarchy are on the query that hierarchy is correct
+refineHierarchy <- function(ranking, corpus.cleaned) {
+  corpus.matrix <- TermDocumentMatrix(corpus.cleaned)
+  
+  hierarchies <- sapply(ranking$original, function(x) 
+      str_split(x, "\\."))
+  last.terms <- sapply(hierarchies, function(x) x[length(x)])
+  
+  last.terms.source <- VectorSource(x=last.terms)
+  last.terms.corpus <- VCorpus(last.terms.source, 
+                         readerControl=list(language="es"))
+  last.terms.cleaned <- cleaningCorpus(last.terms.corpus)
+  
+  terms <- sapply(last.terms.cleaned, function(x)
+    str_split(content(x), " "))
+  
+  holdHierarchy <- numeric()
+  for(i in 1:length(terms)) {
+    num.terms <- length(terms[[i]])
+    freqs <- sapply(terms[[i]], function(x) 
+      tryCatch({inspect(corpus.matrix[x,])[1,1]}, 
+               error=function(err){return(0)}, 
+               finally={print(x)}))
+    freqs.bin <- ifelse(freqs > 0, 1,0)
+    prop <- ifelse(sum(freqs.bin)/num.terms >= 0.6, 1, 0) 
+    holdHierarchy <- rbind(holdHierarchy, prop)
+  }
+  
+}
