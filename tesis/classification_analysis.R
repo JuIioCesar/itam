699948883a22fc4098e96f49ef8b77dd1ef976f7
silil <liliana.millan@gmail.com>
2016-05-30 18:00:01 -0500

    images for dissertation, clasified documents analysis

--- tesis/classification_analysis.R
+++ tesis/classification_analysis.R
@@ -5,10 +5,17 @@ clasificaciones <- tbl(my_db, "clasificaciones")
 
 clasificaciones_df <- collect(clasificaciones)
 
-clasificaciones <- distinct(clasificaciones_df, content)
+clasificaciones <- group_by(clasificaciones_df, content) %>%
+  summarise(bm25=max(bm25),
+            tfidf=max(tfidf))
 
-secciones <- group_by(clasificaciones, seccion) %>%
-  summarise(n=n()) %>% arrange(desc(n))
+s <- inner_join(clasificaciones, clasificaciones_df, by=c("content"="content",
+                                                          "bm25"="bm25")) 
+t <- distinct(s, content, bm25) 
+t <- select(t, content, bm25, tfidf.x, tag_bm25, tag_tfidf, seccion, url)
+names(t)[grep("^tfidf", names(t))] <- "tfidf" 
+clasificaciones <- t
+
 
 ##modificar las de america latina que tienen mexico a editorial nyt
 america_latina <- filter(clasificaciones, seccion=='america-latina') 
@@ -35,4 +42,101 @@ clasificaciones$seccion[grep("http://www.nytimes.com/es/2016/01/25/editorial-las
                              clasificaciones$url)] <- "america-latina"
 
 
-http://www.nytimes.com/es/2016/03/03/la-presion-para-que-renuncie-nicolas-maduro/
+secciones <- group_by(clasificaciones, seccion) %>%
+  summarise(n=n()) %>% arrange(desc(n)) %>%
+  mutate(prop=round(n/sum(n)*100,2))
+secciones$seccion <- factor(secciones$seccion, levels=secciones$seccion)
+
+
+ggplot(secciones, aes(x=seccion, y=prop, fill=seccion)) +
+  geom_bar(stat="identity") +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=90, hjust=1)) +
+  scale_fill_brewer(palette="YlGnBu") +
+  xlab("sección") +
+  ylab("%") +
+  ggtitle("% secciones en el contenido clasificado")
+
+
+clasificaciones$seccion_new <- ifelse(clasificaciones$seccion == "editorial", "nacional",
+                                      ifelse(clasificaciones$seccion =="europa","internacional",
+                                             ifelse(clasificaciones$seccion == "america-latina","internacional",
+                                                    ifelse(clasificaciones$seccion == "financial_times",
+                                                           "internacional",
+                                                           ifelse(clasificaciones$seccion == "tech",
+                                                                  "tecnologia", 
+                                                                  ifelse(clasificaciones$seccion == "mundo",
+                                                                         "internacional", 
+                                                                         clasificaciones$seccion))))))
+
+secciones_nuevas <- group_by(clasificaciones, seccion_new) %>%
+  summarise(n=n()) %>%
+  mutate(prop=round(n/sum(n)*100, 2)) %>%
+  arrange(desc(prop))
+secciones_nuevas$seccion_new <- factor(secciones_nuevas$seccion_new, 
+                                       levels=secciones_nuevas$seccion_new)
+
+ggplot(secciones_nuevas, aes(x=seccion_new, y=prop, fill=seccion_new,
+                             label=n)) +
+  geom_bar(stat="identity") +
+  geom_text(vjust=0, size=3) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=90, hjust=1)) +
+  scale_fill_brewer(palette="YlGnBu") +
+  xlab("sección") +
+  ylab("%") +
+  scale_y_continuous(limits=c(0,50)) +
+  ggtitle("% contenidos por sección")
+
+###documentos clasificados igual
+equal_tags <- filter(clasificaciones, tag_bm25==tag_tfidf)
+equal_tags_agg <- group_by(equal_tags, seccion_new) %>%
+  summarise(n=n()) 
+
+secciones_nuevas_copy <- secciones_nuevas
+secciones_nuevas_copy$seccion_new <- as.character(secciones_nuevas_copy$seccion_new)
+
+totals_secciones <- inner_join(equal_tags_agg, secciones_nuevas_copy, 
+                               by="seccion_new")
+names(totals_secciones) <- c("seccion_new","iguales","totales","prop_totales")
+totals_secciones$prop_iguales <- round(totals_secciones$iguales/
+                                         totals_secciones$totales*100, 2)
+
+
+ggplot(totals_secciones, aes(x=seccion_new, y=prop_iguales, fill=seccion_new,
+                             label=prop_iguales)) +
+  geom_bar(stat="identity") +
+  geom_text(vjust=0, size=3) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=90, hjust=1)) +
+  xlab("sección") +
+  ylab("%") +
+  scale_fill_brewer(palette="YlGnBu") +
+  ggtitle("% de contenidos clasificados\nigual por BM25 y TF/IDF")
+  
+
+###get sample 
+aux <- filter(clasificaciones, seccion_new == "internacional")
+internacional_sample <- sample_n(aux, size=17, replace=F)
+
+aux <- filter(clasificaciones, seccion_new == "nacional")
+nacional_sample <- sample_n(aux, size=14, replace=F)
+
+aux <- filter(clasificaciones, seccion_new == "tecnologia")
+tec_sample <- sample_n(aux, size=7, replace=F)
+
+aux <- filter(clasificaciones, seccion_new == "estados-unidos")
+estados_unidos_sample <- sample_n(aux, size=3, replace=F)
+
+aux <- filter(clasificaciones, seccion_new == "salud")
+salud_sample <- sample_n(aux, size=2, replace=F)
+
+
+#documentos 
+documentos_humanos <- internacional_sample
+documentos_humanos <- rbind(documentos_humanos, nacional_sample)
+documentos_humanos <- rbind(documentos_humanos, tec_sample)
+documentos_humanos <- rbind(documentos_humanos, estados_unidos_sample)
+documentos_humanos <- rbind(documentos_humanos, salud_sample)
+
+save(documentos_humanos, file="docs_formulario.RData")
