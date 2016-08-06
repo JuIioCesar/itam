73e80a03ff3751cffa74d2d5fa8fa03b69d52edb
liliana <liliana.millan@gmail.com>
2016-08-05 20:40:44 -0500

    imagenes resultados

--- tesis/analisis_resultados.R
+++ tesis/analisis_resultados.R
@@ -1,3 +1,8 @@
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tidyr)
+
 #analisis resultados
 
 df <- data.frame(opcion=c("BM25","TF/IDF","Ninguno"),
@@ -41,6 +46,19 @@ df_ninguno <- inner_join(all_docs, docs_nin, by=c("seccion_new"="seccion_new"))
 names(df_ninguno) <- c("seccion","all","ninguno")
 df_ninguno$prop <- round(df_ninguno$ninguno/df_ninguno$all*100,2)
 
+ggplot(df_ninguno, aes(x=seccion, y=prop, fill=seccion, label=prop)) +
+  geom_bar(stat="identity") +
+  geom_text(vjust=0) +
+  geom_hline(yintercept = sum(df_ninguno$ninguno)/sum(df_ninguno$all)*100, color="black") +
+  theme_bw() +
+  ggtitle("% de selección de etiquetas sugeridas\npor BM25 en las encuestas") +
+  scale_fill_brewer(palette="Spectral") +
+  ylab("%") +
+  scale_y_continuous(limits=c(0, (max(df_ninguno$prop)*.10 + max(df_ninguno$prop)))) +
+  theme(axis.text.x=element_text(angle=90, hjust=1))
+
+  
+
 
 ##bm25
 docs_bm25 <- documentos_humanos[bm25,]
@@ -73,3 +91,30 @@ df_tfidf <- inner_join(all_docs, docs_tfidf_df, by=c("seccion_new"="seccion_new"
 names(df_tfidf) <- c("seccion","all","tfidf")
 df_tfidf$prop <- round(df_tfidf$tfidf/df_tfidf$all*100,2)
 
+ggplot(df_tfidf, aes(x=seccion, y=prop, fill=seccion, label=prop)) +
+  geom_bar(stat="identity") +
+  geom_text(vjust=0) +
+  geom_hline(yintercept = sum(df_tfidf$tfidf)/sum(df_tfidf$all)*100, color="black") +
+  theme_bw() +
+  ggtitle("% de selección de etiquetas sugeridas\npor TF/IDF en las encuestas") +
+  scale_fill_brewer(palette="Spectral") +
+  ylab("%") +
+  scale_y_continuous(limits=c(0, (max(df_tfidf$prop)*.10 + max(df_tfidf$prop)))) +
+  theme(axis.text.x=element_text(angle=90, hjust=1))
+
+
+##all in one
+recomendaciones <- data.frame(seccion=df_bm25$seccion,
+                              all=df_bm25$all,
+                              bm25=df_bm25$prop,
+                              tfidf=df_tfidf$prop,
+                              ninguno=df_ninguno$prop)
+
+recom <- gather(recomendaciones, modelo, prop, -seccion, -all)
+
+ggplot(recom, aes(x=seccion, y=prop, fill=modelo)) +
+  geom_bar(stat="identity", position="stack") +
+  scale_fill_brewer(palette="YlGnBu") +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=90, hjust=1)) +
+  ggtitle("% recomendaciones seleccionadas por sección")
